0e5f910429da721e130d0cc8ca7e1867904c6a48
Ailin Chua <ai-chua@outlook.com>
2020-09-09 22:49:35 +0800

    Seeded Artists from API

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# CALL AN API AND UPDATE THE DB
+# GET ALL 275
+# set url to the api
+# loop to call the api until the api response has no key "offset"
+  # each loop
+  # get data
+  # -> key `records`
+  # -> key `fields`
+  # -> key `name`
+  # create an artist instance => Artist.new
+  # save it to the DB => instance.save
+require 'open-uri'
+require 'pry-byebug'
+
+base_url = 'https://api.airtable.com/v0/appOnpFap3mruBMNc/artists?api_key=key5wiIECTI6NOYbO'
+artists = JSON.parse(open(base_url).read)
+offset = artists['offset']
+# FIRST OFFSET
+
+artists['records'].each do |data|
+  Artist.create!(name: data['fields']['name'])
+end
+
+# CONCAT 2ND OFFSET WITH BASE URL
+# LOOP TO 2ND OFFSET
+while offset
+  artists = JSON.parse(open("#{base_url}&offset=#{offset}").read)
+  offset = artists['offset']
+  artists['records'].each do |data|
+    Artist.create!(name: data['fields']['name'])
+  end
+end
